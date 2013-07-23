c8f67e7447db9f4edd93096e2f958b3e9ec43ceb
Sam Livingston-Gray <geeksam@gmail.com>
2013-07-23 09:32:57 -0700

    I have a feeling we'll be seeing more of these...

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,8 +1,9 @@
 class Bob
   def hey(message_of_critical_importance)
-    if shouty?(message_of_critical_importance)
+    case
+    when shouty?(message_of_critical_importance)
       "Woah, chill out!"
-    elsif asking_a_question?(message_of_critical_importance)
+    when asking_a_question?(message_of_critical_importance)
       "Sure."
     else
       "Whatever."
